2a9f4d7328dacf5f2f0b8e7ab7da4aec9483b0ca
BielHaard <julio.garcia.profissional@gmail.com>
2023-03-09 21:48:21 -0300

    atualização

--- src/main/java/com/julio/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/julio/bookstoremanager/BookstoremanagerApplication.java
@@ -1,7 +1,9 @@
 package com.julio.bookstoremanager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookstoremanagerApplication {
---
+++ src/main/java/com/julio/bookstoremanager/customInfoEndpoint/infoWebEndpointExtension.java
@@ -0,0 +1,32 @@
+package com.julio.bookstoremanager.customInfoEndpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@EndpointWebExtension(endpoint = InfoEndpoint.class)
+public class infoWebEndpointExtension {
+    @Autowired
+    private InfoEndpoint delegate;
+
+    @ReadOperation
+    public WebEndpointResponse<Map> info(){
+        Map<String, Object> info = this.delegate.info();
+        Integer status = getStatus(info);
+        Map<String, Object> customInfo = new HashMap<>(info);
+        customInfo.put("customInfo", "handOn");
+        return new WebEndpointResponse<>(customInfo, status);
+    }
+
+    private Integer getStatus(Map<String, Object> info) {
+        return 200;
+    }
+
+}
